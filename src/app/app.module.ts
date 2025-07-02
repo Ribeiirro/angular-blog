115deea0e1490405afdd483fc8b6d5307ad3c2e3
José Ribeiro <82005318@marabraz.com.br>
2025-07-02 09:34:49 -0300

    feat: novo visual do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 
 
 @NgModule({
@@ -19,7 +20,7 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
