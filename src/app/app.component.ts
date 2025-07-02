91b172103bf550ca4d833e4c5b4254f2da623976
José Ribeiro <joseribeirocsr@gmail.com>
2025-07-02 09:47:29 -0300

    standalone step 3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: false
+    imports: [RouterOutlet, MenuBarComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
